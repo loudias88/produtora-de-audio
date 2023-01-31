23509971de9989fc9f2fddd23d88c7a0b0e68619
loudias88 <luisheleno.dias@gmail.com>
2023-01-31 20:15:15 -0300

    build global styles

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Nav, NavbarContainer } from './Navbar.elements';
+
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar;
